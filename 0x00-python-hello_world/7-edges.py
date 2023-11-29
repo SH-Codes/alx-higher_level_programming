0d88f321770a616014c0c34403a28e340f1a8a67
SH-Codes <wldrms@outlook.com>
2023-11-29 14:55:24 +0200

    7-edges.py prints First 3 letters: Hol, Last 2 letters: on and Middle word: olberto

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = "First 2 letters: Hol"
+word_first_3 = "First 3 letters: Hol"
 word_last_2 = "Last 2 letters: on"
 middle_word = "Middle word: olberto"
 print(f"First 3 letters: {word_first_3}")
