5856105a39df135e7ef662782de1f38a964e4d96
SH-Codes <wldrms@outlook.com>
2024-01-08 12:31:03 +0200

    3-say_my_name.py - updated

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,15 +5,14 @@
 
 def say_my_name(first_name, last_name=""):
     """Print a name.
-
     Args:
         first_name (str): The first name to print.
-        last_name (str, optional): The last name to print.
-
+        last_name (str): The last name to print.
     Raises:
-        TypeError: If either first_name or last_name are not strings.
+        TypeError: If either of first_name or last_name are not strings.
     """
-    if not all(isinstance(name, str) for name in (first_name, last_name)):
-        raise TypeError("Both first_name and last_name must be strings")
-    
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
