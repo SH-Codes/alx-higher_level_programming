dec389846eafb30fcadc7217a0b681f14f7159f6
SH-Codes <wldrms@outlook.com>
2024-02-15 02:08:15 +0200

    3-list_tables.sql - shows all tables from a database

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,6 @@
 -- List all tables from a database
 -- in this example the name of the datebase is 'mysql'
 
-SHOW TABLES FROM MYSQL;
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'mysql';
