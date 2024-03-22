d23c7637af4b12fc81743a054f9ec9a6ee20a332
SH-Codes <wldrms@outlook.com>
2024-03-22 21:21:22 +0200

    102-relationship_cities_states_list.py - ists all City objects from the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -4,18 +4,26 @@
 import sys
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import relationship
-
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        for city_ins in instance.cities:
-            print(city_ins.id, city_ins.name, sep=": ", end="")
-            print(" -> " + instance.name)
+
+    state_name = input("Enter the state name: ")
+
+    state = session.query(State).filter_by(name=state_name).first()
+    if state:
+        print("Cities in {}: ".format(state_name))
+        for city in state.cities:
+            print(f"{city.id}: {city.name}")
+    else:
+        print("State not found.")
