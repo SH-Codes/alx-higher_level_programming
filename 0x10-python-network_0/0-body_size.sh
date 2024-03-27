7c962d582197191f59501094d5d9b26539b5142e
SH-Codes <wldrms@outlook.com>
2024-03-27 23:50:04 +0200

    0-body_size.sh - takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-curl -s "$" | wc -c
+
+# Check if URL is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send request to URL and count the size of the response body in bytes
+curl_output=$(curl -s "$1")
+byte_count=$(echo -n "$curl_output" | wc -c)
+
+echo "$byte_count"
