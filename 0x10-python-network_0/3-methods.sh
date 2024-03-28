ac8badb91eacb66dcbb70a3b420b786cf2ca0d37
SH-Codes <wldrms@outlook.com>
2024-03-28 04:27:39 +0200

    3-methods.sh - takes in a URL and displays all HTTP methods the server will accept.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/bash
-# Display all http methods
+# Display all HTTP methods allowed by the provided URL
 
-# Uses curl to send a HEAD request to the provided URL
+# Check if URL is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a HEAD request to the provided URL using curl
 # -s: Silent mode, hides the progress meter
 # -I: Send a HEAD request instead of a GET request, to retrieve only the headers
 # "$1": Takes the first argument provided to the script as the URL
 # Then grep extracts the "Allow" header, which lists allowed HTTP methods
-# Finally, cut is used to extract the HTTP methods from the header
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# Finally, awk is used to print all HTTP methods listed in the header
+curl -sI "$1" | grep -i "Allow" | awk '{print substr($0, index($0,$2))}'
