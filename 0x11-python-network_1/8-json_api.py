ef53b114d53ab5302e8334fbd6b757d34a3f0613
SH-Codes <wldrms@outlook.com>
2024-03-30 10:20:48 +0200

    8-json_api.py - takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
 Usage: ./8-json_api.py <letter>
   - The letter is sent as the value of the variable `q`.
@@ -7,29 +7,17 @@ Usage: ./8-json_api.py <letter>
 import sys
 import requests
 
-def main():
-    if len(sys.argv) > 2:
-        print("Usage: ./8-json_api.py <letter>")
-        sys.exit(1)
-
-    letter = sys.argv[1] if len(sys.argv) == 2 else ""
-    payload = {"q": letter}
-
-    try:
-        response = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-
-        json_response = response.json()
-        if not json_response:
-            print("No result")
-        else:
-            print("[{}] {}".format(json_response.get("id"), json_response.get("name")))
-    except requests.RequestException as e:
-        print(f"Error occurred: {e}")
-        sys.exit(1)
-    except ValueError:
-        print("Not a valid JSON")
-        sys.exit(1)
 
 if __name__ == "__main__":
-    main()
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
