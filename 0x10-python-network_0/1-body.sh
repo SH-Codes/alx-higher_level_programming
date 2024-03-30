dddaa319b45106dab9e2484b9584b84086055b6b
SH-Codes <wldrms@outlook.com>
2024-03-30 06:31:00 +0200

    sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-
-# Uses curl to send a GET request to the provided URL
-# -s: Silent mode, hides the progress meter
-# -L: Follow redirects if the server redirects the request
-# "$1": Takes the first argument provided to the script as the URL
 curl -sL "$1"
