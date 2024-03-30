e4243e493a1a7b8a97d77543e690e00c0393190d
SH-Codes <wldrms@outlook.com>
2024-03-30 10:39:17 +0200

    finds a peak in a list of unsorted integers.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
+
 
 def find_peak(list_of_integers):
     """
-    Finds a peak in a list of unsorted integers using a modified binary search.
-
     Args:
-        list_of_integers (list): List of integers to find a peak of.
-
-    Returns:
-        int or None: Peak of list_of_integers if found, None otherwise.
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
     """
     size = len(list_of_integers)
-    # Edge case: Empty list
+    mid_e = size
+    mid = size // 2
+
     if size == 0:
         return None
-    # Perform binary search to find peak
-    left, right = 0, size - 1
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            # Peak must be on the right side
-            left = mid + 1
+
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
         else:
-            # Peak must be on the left side (or at mid)
-            right = mid
-    # Peak found at left or right index
-    return list_of_integers[left]
+            return list_of_integers[mid]
