93f775d7adae26476a2eabaac7c8f9467c63cf7e
SH-Codes <wldrms@outlook.com>
2024-03-30 09:30:01 +0200

    5-hbtn_header.py - sends a request to the URL and displays the value of the variable X-Request-Id in the response header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Displays the X-Request-Id header variable of a request to a given URL.
 Usage: ./5-hbtn_header.py <URL>
 """
 import sys
 import requests
 
-if __name__ == "__main__":
-    url = sys.argv[1]
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: ./5-hbtn_header.py <URL>")
+        sys.exit(1)
 
-    r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    print(response.headers.get("X-Request-Id"))
+
+if __name__ == "__main__":
+    main()
