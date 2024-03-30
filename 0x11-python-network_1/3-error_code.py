dff713d5e4c6c10c7ce5ecef30c57dad4ebe625e
SH-Codes <wldrms@outlook.com>
2024-03-30 08:56:14 +0200

    3-error_code.py - sends a request to the URL and displays the body of the response (decoded in utf-8).

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Sends a request to a given URL and displays the response body.
 Usage: ./3-error_code.py <URL>
   - Handles HTTP errors.
@@ -7,19 +7,13 @@ import sys
 import urllib.error
 import urllib.request
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: ./3-error_code.py <URL>")
-        sys.exit(1)
 
+if __name__ == "__main__":
     url = sys.argv[1]
 
+    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(request) as response:
             print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
-        sys.exit(1)
-
-if __name__ == "__main__":
-    main()
