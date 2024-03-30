069347e09efb4f6688bc83b8b9d29999ec840bf8
SH-Codes <wldrms@outlook.com>
2024-03-30 06:46:48 +0200

    5-post_params.sh - sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided
-[ -z "$1" ] && { echo "Usage: $0 <URL>"; exit 1; }
-
 # Send POST request with specified data and display response body
 curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
