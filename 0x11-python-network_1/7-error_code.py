5c7d7e853e19f1db9650e6cd738bc916c85295cd
SH-Codes <wldrms@outlook.com>
2024-03-30 10:14:50 +0200

    7-error_code.py - sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,20 +6,11 @@ Usage: ./7-error_code.py <URL>
 import sys
 import requests
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: ./7-error_code.py <URL>")
-        sys.exit(1)
-
+if __name__ == "__main__":
     url = sys.argv[1]
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        print(response.text)
-    except requests.RequestException as e:
-        print(f"Error occurred: {e}")
-        sys.exit(1)
-
-if __name__ == "__main__":
-    main()
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
