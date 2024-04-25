f75a1195db19d0bb7d9505662c64985547f28585
SH-Codes <wldrms@outlook.com>
2024-04-25 05:01:29 +0200

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,41 +2,25 @@
 
 const request = require('request');
 
-const getCharacter = (characterUrl) => {
-  return new Promise((resolve, reject) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  console.log(`Characters in ${movie.title}:`);
+  movie.characters.forEach((characterUrl) => {
+    request(characterUrl, (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error(charError);
         return;
       }
-      const character = JSON.parse(body);
-      resolve(character.name);
+      const character = JSON.parse(charBody);
+      console.log(character.name);
     });
   });
-};
-
-const getMovieCharacters = async (movieId) => {
-  const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-  try {
-    const response = await new Promise((resolve, reject) => {
-      request(apiUrl, (error, response, body) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        resolve(JSON.parse(body));
-      });
-    });
-
-    console.log(`Characters in ${response.title}:`);
-    for (const characterUrl of response.characters) {
-      const characterName = await getCharacter(characterUrl);
-      console.log(characterName);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const movieId = process.argv[2];
-getMovieCharacters(movieId);
+});
